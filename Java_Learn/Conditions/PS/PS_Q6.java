1df8a40af8990f650f550ea23aa31efca5f2dcb2
Piyush Das <piyush.kdas117@gmail.com>
2025-10-03 20:59:34 +0530

    Added detailed comments for clarity - no logic changes

--- Java_Learn/Conditions/PS/PS_Q6.java
+++ Java_Learn/Conditions/PS/PS_Q6.java
@@ -1,33 +1,47 @@
-//Trying to find from the url
-//.com --> commercial website
-//.org -> organisation website
-//.in -> India Website
+//Website Domain Identifier
+//Program to identify website type based on domain extension
+// .com --> commercial website
+// .org -> organisation website
+// .in -> India Website
 
+// Package declaration - organizes the class into a folder structure
 package Java_Learn.Conditions.PS;
 
+// Import Scanner class to read user input from console
 import java.util.Scanner;
 
+// Main class definition
 public class PS_Q6 {
     public static void main(String[] args) {
-
-        //Creating Scanner Class
+        // Create Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
-
-        //Taking Input
+        
+        // Prompt user to enter a domain extension (like .com, .org, .in)
         System.out.print("Enter domain name: ");
+        
+        // Read the domain extension entered by user as a string
         String domain = sc.next();
-
-        //Condition and printing
+        
+        // Use switch statement to check domain type
+        // Switch is efficient for checking one variable against multiple fixed values
         switch (domain) {
+            // Case 1: If user enters ".com" - indicates a commercial website
             case ".com":
                 System.out.print("Commercial Website...");
-                break;
+                break;  // Break exits the switch statement after executing this case
+            
+            // Case 2: If user enters ".org" - indicates an organization website
             case ".org":
                 System.out.println("Organisation Website...");
-                break;
+                break;  // Break prevents fall-through to next case
+            
+            // Case 3: If user enters ".in" - indicates an Indian website
             case ".in":
                 System.out.println("Indian Website...");
-                break;
+                break;  // Break stops further case checking
+            
+            // Default case: If user enters any other domain extension
+            // This executes when none of the above cases match
             default:
                 System.out.println("No recognized domain");
         }
