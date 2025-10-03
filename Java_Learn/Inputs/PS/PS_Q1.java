c394a104b95b09b6706b6d0cb367395c216df676
Piyush Das <piyush.kdas117@gmail.com>
2025-10-03 21:15:28 +0530

    Update PS_Q1.java to sum three numbers

--- Java_Learn/Inputs/PS/PS_Q1.java
+++ Java_Learn/Inputs/PS/PS_Q1.java
@@ -1,4 +1,4 @@
-//Write a program to sum three numbers in Java
+// Program to sum three numbers entered by the user
 
 package Java_Learn.Inputs.PS;
 
@@ -6,14 +6,19 @@ import java.util.Scanner;
 
 public class PS_Q1 {
     public static void main(String[] args) {
+        // Prompt the user to enter three numbers
         System.out.println("Enter three numbers:");
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); // Create Scanner object for input
+
+        // Read three integer numbers from the user
         int num_1 = sc.nextInt();
         int num_2 = sc.nextInt();
         int num_3 = sc.nextInt();
 
+        // Calculate the sum of the three numbers
         int sum = num_1 + num_2 + num_3;
 
+        // Output the result
         System.out.println("Sum = " + sum);
     }
 }
