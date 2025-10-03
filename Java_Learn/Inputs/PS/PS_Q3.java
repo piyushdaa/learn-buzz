07ad9fee9a9f9a43b92e7d0dc1254c755b0ff0d5
Piyush Das <piyush.kdas117@gmail.com>
2025-10-03 21:17:11 +0530

    Add descriptive comments for user input and output in PS_Q3.java

--- Java_Learn/Inputs/PS/PS_Q3.java
+++ Java_Learn/Inputs/PS/PS_Q3.java
@@ -1,4 +1,4 @@
-//WAP to greet the user with "Hello <user>, have a good day text".
+// Program to greet the user with "Hello <user>, have a good day"
 
 package Java_Learn.Inputs.PS;
 
@@ -6,11 +6,13 @@ import java.util.Scanner;
 
 public class PS_Q3 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); // Create Scanner object for input
+
+        // Ask for user's name
         System.out.print("Enter your name: ");
         String name = sc.next();
+
+        // Display greeting message
         System.out.printf("Hello %s, Have A Good Day!", name);
-
-
     }
 }
