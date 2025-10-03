e50c92a563695751b67f06fbee47046f1b372335
Piyush Das <piyush.kdas117@gmail.com>
2025-10-03 21:18:56 +0530

    Update PS_Q5.java to check if user input is an integer

--- Java_Learn/Inputs/PS/PS_Q5.java
+++ Java_Learn/Inputs/PS/PS_Q5.java
@@ -1,14 +1,21 @@
+// Program to check if the user input is an integer
+
 package Java_Learn.Inputs.PS;
 
 import java.util.Scanner;
 
 public class PS_Q5 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); // Scanner for input
+
+        // Prompt user to enter a value
         System.out.print("Enter a integer: ");
+
+        // Check whether the next input is an integer
         boolean integer = sc.hasNextInt();
+
+        // Display result
         System.out.println("Is this number an integer?");
         System.out.println(integer);
-
     }
 }
