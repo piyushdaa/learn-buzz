2925f8fca676c22c898e02966164f96006457e1c
Piyush Das <piyush.kdas117@gmail.com>
2025-10-03 21:14:56 +0530

    Updated Ex_01.java with new content

--- Java_Learn/Inputs/Ex_01.java
+++ Java_Learn/Inputs/Ex_01.java
@@ -1,4 +1,4 @@
-//WAP to calculate marks in % for 5 subjects. (100 marks/subject)
+// Program to calculate percentage marks for 5 subjects (each out of 100)
 
 package Java_Learn.Inputs;
 
@@ -11,6 +11,7 @@ public class Ex_01 {
 
         Scanner marks = new Scanner(System.in);
 
+        // Input subject names
         System.out.println("Enter subjects... ");
 
         System.out.print("\nSubject 1: ");
@@ -28,6 +29,7 @@ public class Ex_01 {
         System.out.print("Subject 5: ");
         String sub_5 = marks.next();
 
+        // Input marks for each subject
         System.out.println("\nEnter Marks (out of 100)...");
 
         System.out.print(sub_1 + " : ");
@@ -45,8 +47,11 @@ public class Ex_01 {
         System.out.print(sub_5 + " : ");
         int marks_5 = marks.nextInt();
 
+        // Calculate total marks and percentage
         double sum = marks_1 + marks_2 + marks_3 + marks_4 + marks_5;
         double total_marks = (sum / 500) * 100;
+
+        // Display the percentage
         System.out.println("Marks: " + total_marks + "%");
     }
 }
